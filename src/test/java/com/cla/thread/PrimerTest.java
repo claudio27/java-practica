c5b5319785bf3afa91c7dc77d45d2de05262dd00
cserrano <cserrano@alaya.cl>
2017-11-13 19:49:34 -0300

    probando junit

---
+++ src/test/java/com/cla/thread/PrimerTest.java
@@ -0,0 +1,14 @@
+package com.cla.thread;
+
+import org.junit.Test;
+
+/**
+ * Created by alaya on 13/11/17.
+ */
+public class PrimerTest {
+
+    @Test
+    public void pruebaTest(){
+        System.out.println("hola mundo");
+    }
+}
